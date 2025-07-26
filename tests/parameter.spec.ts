c7c208a02a1bec14052f67a8c79b85756db75d07
Vinayak M <vinayakhk9@gmail.com>
2025-07-26 19:30:45 +0530

    final commit for yml file

--- tests/parameter.spec.ts
+++ tests/parameter.spec.ts
@@ -10,3 +10,39 @@ import {test, expect} from '@playwright/test';
     await expect(page.getByRole('heading')).toHaveText(expected);
   });
 });
+
+[
+  { name: 'Alice', expected: 'Hello, Alice!' },
+  { name: 'Bob', expected: 'Hello, Bob!' },
+  { name: 'Charlie', expected: 'Hello, Charlie!' },
+].forEach(({ name, expected }) => {
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+  test(`testing with1 ${name}`, async ({ page }) => {
+    await page.goto(`https://example.com/greet?name=${name}`);
+    await expect(page.getByRole('heading')).toHaveText(expected);
+  });
+});
+
+[
+  { name: 'Alice', expected: 'Hello, Alice!' },
+  { name: 'Bob', expected: 'Hello, Bob!' },
+  { name: 'Charlie', expected: 'Hello, Charlie!' },
+].forEach(({ name, expected }) => {
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+  test(`testing with2 ${name}`, async ({ page }) => {
+    await page.goto(`https://example.com/greet?name=${name}`);
+    await expect(page.getByRole('heading')).toHaveText(expected);
+  });
+});
+
+[
+  { name: 'Alice', expected: 'Hello, Alice!' },
+  { name: 'Bob', expected: 'Hello, Bob!' },
+  { name: 'Charlie', expected: 'Hello, Charlie!' },
+].forEach(({ name, expected }) => {
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+  test(`testing with3 ${name}`, async ({ page }) => {
+    await page.goto(`https://example.com/greet?name=${name}`);
+    await expect(page.getByRole('heading')).toHaveText(expected);
+  });
+});
